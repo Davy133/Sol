cc1a8645735611ccb5fc936dd0fb72220103396a
AnthonyWilly <twilly2004@gmail.com>
2026-03-02 17:22:27 -0300

    fix: trata caracteres de controle no terminal_putchar
    Adiciona blocos condicionais que tratam os caracteres de controle '\n', '\r' e '\t'.
    
    Closes #11

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -5,4 +5,12 @@
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+  printf("Testando quebra de linha:\n");
+  printf("Linha 1\nLinha 2\nLinha 3\n");
+    
+  printf("\nTestando o Tab:\n");
+  printf("Coluna 1\tColuna 2\tColuna 3\n");
+    
+  printf("\nTestando o Carriage Return:\n");
+  printf("Isso vai sumir\rIsso vai ficar\n");
 }
