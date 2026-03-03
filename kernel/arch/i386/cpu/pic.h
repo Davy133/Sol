61bac19cef008b6bfb02b1c999a8741c8584f1ec
Salomão Davy <salomao.davy.santos.coelho@ccc.ufcg.edu.br>
2026-03-03 09:50:41 -0300

    feat: implement serial port driver and integrate with kernel

--- kernel/arch/i386/cpu/pic.h
+++ kernel/arch/i386/cpu/pic.h
@@ -1,7 +1,7 @@
 #ifndef PIC_H
 #define PIC_H
 
-#include <stdint.h>
+#include <kernel/io.h>
 
 #define PIC1_COMMAND    0x20
 #define PIC1_DATA       0x21
@@ -12,16 +12,6 @@
 
 #define PIC1_OFFSET     0x20    /* IRQs 0-7  -> IDT 32-39  */
 #define PIC2_OFFSET     0x28    /* IRQs 8-15 -> IDT 40-47  */
-
-static inline void outb(uint16_t port, uint8_t val) {
-    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
 
 void pic_remap(int offset1, int offset2);
 void pic_send_eoi(uint8_t irq);
